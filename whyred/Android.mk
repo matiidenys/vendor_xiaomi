48cbd81e59391bc5e3b1af9637ad8175c737e211
Денис <50969017+matyidenisz11@users.noreply.github.com>
2019-05-29 21:24:22 +0300

    Update Android.mk

--- whyred/Android.mk
+++ whyred/Android.mk
@@ -247,9 +247,9 @@ LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := SwiftKey
-LOCAL_MODULE_OWNER := SwiftKey
-LOCAL_SRC_FILES := proprietary/priv-app/SwiftKey/SwiftKey.apk
+LOCAL_MODULE := Poweramp
+LOCAL_MODULE_OWNER := Poweramp
+LOCAL_SRC_FILES := proprietary/priv-app/Poweramp/Poweramp.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
